b317eaca3dcee6098626983e71750ac15fc373ba
Richard Matheus <richard.ataliba.711@ufrn.edu.br>
2024-06-19 19:28:13 -0300

    Adicionando form de pesquisa por nome e escola

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { usePagination } from '../hooks/usePagination'
-
+import { usePagination } from '../hooks/usePagination';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
 import Loading from './Loading';
 import './Projetos.css';
 
-const Projetos = ({ ano, onTotalProjectsChange }) => {
-
+const Projetos = ({ ano, onTotalProjectsChange, searchName, searchEscola }) => {
     const [data, setData] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
     const {
@@ -20,36 +17,36 @@ const Projetos = ({ ano, onTotalProjectsChange }) => {
     } = usePagination(filteredData, 4);
 
     useEffect(() => {
-
-        // Sou iniciante em react e posso não está fazendo tudo da melhor forma possível
-        // Lógica desse useEffect => O primerio if é para criar a primeira e única requisição desssa página 
-        // ele só acontece quando data é o padrão state que eu defino
-        // Após isso eu apenas dou o check se ano chegou do componente pai como todos os anos, se for isso eu passo
-        // data direto no filter data, caso não seja, eu passo uma data filtrada no filterdata
-
-        if (data.length == 0) {
-
-            console.log('é igual a 0')
-
+        if (data.length === 0) {
             axios.get('http://localhost:8000/projetos')
                 .then(response => {
                     setData(response.data);
-
                 })
                 .catch(error => {
                     console.error('Error fetching data:', error);
-                    setFilteredData(data)
-                })
+                });
         }
 
-        setFilteredData(ano === "todos os anos" ? data : data.filter(item => item.ano === String(ano)));
+        let filtered = ano === "todos os anos" ? data : data.filter(item => item.ano === String(ano));
 
-    }, [ano, data]);
+        if (searchEscola) {
+            filtered = filtered.filter(item => item.escola.toLowerCase().includes(searchEscola.toLowerCase()));
+        }
+
+        if (searchName) {
+            filtered = filtered.filter(item => item.nome.toLowerCase().includes(searchName.toLowerCase()));
+        }
+
+        setFilteredData(filtered)
+
+    }, [ano, data, searchName, searchEscola]);
 
     useEffect(() => {
-        onTotalProjectsChange(filteredData.length)
-    }, [onTotalProjectsChange, filteredData])
+        onTotalProjectsChange(filteredData.length);
+    }, [onTotalProjectsChange, filteredData]);
 
+    const nameExiste = !!searchName;
+    const escolaExiste = !!searchEscola;
 
     return data.length < 1 ? (
         <div className='loading'>
@@ -82,12 +79,12 @@ const Projetos = ({ ano, onTotalProjectsChange }) => {
                 {getItemsPage().map(item => (
                     <div className="card" key={item.id}>
                         <Link to={`/projeto?id=${item.id}`}>
-                            <h3>{item.nome}</h3>
-                            <p className='item_p'><strong>Categoria de premiação:</strong> {item.categoria_premiacao}</p>
-                            <p><strong>Escola:</strong> {item.escola}</p>
-                            <p className='item_p'><strong>Cidade:</strong> {item.cidade}</p>
-                            <p className='item_p'><strong>Estado:</strong> {item.estado}</p>
-                            <p className='item_p'><strong>Ano:</strong> {item.ano}</p>
+                            <h3 className={nameExiste?'destaque-pesquisa':''}>{item.nome}</h3>
+                            <p ><strong>Categoria de premiação:</strong> {item.categoria_premiacao}</p>
+                            <p className={escolaExiste?'destaque-pesquisa':''}><strong>Escola:</strong> {item.escola}</p>
+                            <p ><strong>Cidade:</strong> {item.cidade}</p>
+                            <p ><strong>Estado:</strong> {item.estado}</p>
+                            <p ><strong>Ano:</strong> {item.ano}</p>
                             <p></p>
                             <hr />
                         </Link>
--- src/routers/pagProjetos.jsx
+++ src/routers/pagProjetos.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
-
 import Projetos from '../components/Projetos'
-
 import "./pagProjetos.css"
 
-const home = () => {
-
+const Home = () => {
     const [ano, setAno] = useState("todos os anos")
     const [totalPag, setTotalPag] = useState(1080)
+
+    const [searchName, setSearchName] = useState('');
+    const [searchEscola, setSearchEscola] = useState('');
 
     const totalproje = (totalPag) => {
         setTotalPag(totalPag)
@@ -15,27 +15,54 @@ const home = () => {
 
     return (
         <div className='pagProjetos'>
-            <div className='mainProjetos'>
-                <h1>Febrace - Projetos premiados  </h1> <h1 id='ano'>{ano}</h1>
-            </div><br />
-            <div className='qntdProjetos'>
-                <h3 id='projetos-total'>{totalPag}</h3> <h3>projetos</h3>
+            <div className='cabecalho'>
+                <div className='filtro_pesquisa'>
+                    <h1>Pesquise por nome ou escola do projeto</h1>
+                    <form action="post">
+                        <div id='searchname'>
+                            <label htmlFor="">Nome do projeto:</label>
+                            <input
+                                type="text"
+                                placeholder='Digite um nome de projeto'
+                                value={searchName}
+                                onChange={e => setSearchName(e.target.value)}
+                            />
+                        </div>
+                        <div id='searchescola'>
+                            <label htmlFor="">Escola do projeto:</label>
+                            <input
+                                type="text"
+                                placeholder='Digite uma escola'
+                                value={searchEscola}
+                                onChange={e => setSearchEscola(e.target.value)}
+                            />
+                        </div>
+                    </form>
+                </div>
+                <div className='filtro_anos'>
+                    <div className='mainProjetos'>
+                        <h1>Febrace - Projetos premiados  </h1> <h1 id='ano'>{ano}</h1>
+                    </div><br />
+                    <div className='qntdProjetos'>
+                        <h3 id='projetos-total'>{totalPag}</h3> <h3>projetos</h3>
+                    </div>
+                    <div className="button-container">
+                        <input type="button" value="Todos os anos" onClick={() => setAno("todos os anos")} />
+                        <input type="button" value="2023" onClick={() => { setAno(2023) }} />
+                        <input type="button" value="2022" onClick={() => { setAno(2022) }} />
+                        <input type="button" value="2021" onClick={() => { setAno(2021) }} />
+                        <input type="button" value="2020" onClick={() => { setAno(2020) }} />
+                        <input type="button" value="2019" onClick={() => { setAno(2019) }} />
+                        <input type="button" value="2018" onClick={() => { setAno(2018) }} />
+                    </div>
+                </div>
             </div>
-            <div className="button-container">
-                <input type="button" value="Todos os anos" onClick={() => setAno("todos os anos")} />
-                <input type="button" value="2023" onClick={() => { setAno(2023) }} />
-                <input type="button" value="2022" onClick={() => { setAno(2022) }} />
-                <input type="button" value="2021" onClick={() => { setAno(2021) }} />
-                <input type="button" value="2020" onClick={() => { setAno(2020) }} />
-                <input type="button" value="2019" onClick={() => { setAno(2019) }} />
-                <input type="button" value="2018" onClick={() => { setAno(2018) }} />
-            </div>
+            <hr />
             <div className='home'>
-                <Projetos ano={ano} onTotalProjectsChange={totalproje}></Projetos>
+                <Projetos ano={ano} searchEscola={searchEscola} searchName={searchName} onTotalProjectsChange={totalproje} />
             </div>
         </div>
     )
 }
 
-
-export default home
+export default Home;
