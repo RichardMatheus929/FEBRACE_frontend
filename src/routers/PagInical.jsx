36046726c4dd8438ccd2d4af46a18427c7f106d4
Richard Matheus <richard.ataliba.711@ufrn.edu.br>
2024-06-25 21:15:40 -0300

    teste

--- src/routers/PagInical.jsx
+++ src/routers/PagInical.jsx
@@ -8,6 +8,7 @@ const PagInical = () => {
     return (
         <div className='PagInicial'>
             <div className='central'>
+                <h1>teste</h1>
                 <p className="txt-inicial">Bem-vindo(a), essa é a página inicial da FEBRACE!</p>
                 <p className='descricao'>Aqui você terá acesso ao Painel de Dados dos projetos premiados da FEBRACE! </p>
                 <p>Vamos lá?</p>
